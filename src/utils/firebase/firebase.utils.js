ca9a6f64796ee9a67c2f7a9ff753a6b2d0921ee8
Kamil <kamil.michalik@stanusch.com>
2022-10-03 16:09:01 +0200

    added our sign in component along with firebase utils and firebase libraries

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,73 @@
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider
+} from 'firebase/auth'
+
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrb0BYOK7dP_qV8cTnfkuxmVzFPrrbf2s",
+  authDomain: "react-course-crwn-db-b6f3a.firebaseapp.com",
+  projectId: "react-course-crwn-db-b6f3a",
+  storageBucket: "react-course-crwn-db-b6f3a.appspot.com",
+  messagingSenderId: "397854688840",
+  appId: "1:397854688840:web:0f7975a864ec26ce053a7d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  console.log('userDocRef', userDocRef);
+  const userSnapshot = await getDoc(userDocRef);
+  console.log('userSnapshot', userSnapshot);
+  console.log('userSnapshot exists?', userSnapshot.exists());
+
+  const {displayName, email} = userAuth;
+  const createdAt = new Date();
+  const lastLoginAt = new Date();
+
+  if (!userSnapshot.exists()) {
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        lastLoginAt
+      })
+    } catch (e) {
+      console.log('error creating ther user', e.message)
+    }
+  } else {
+    try {
+      await setDoc(userDocRef, {
+        lastLoginAt
+      })
+    } catch (e) {
+      console.log('error updating the user', e.message)
+    }
+  }
+
+  return userDocRef;
+}
